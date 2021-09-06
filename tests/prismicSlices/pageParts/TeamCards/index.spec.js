bbb11f126a1333a232b7bcae0d72684f8739777a
denisoed <denisoed@gmail.com>
2021-09-06 16:43:32 +0600

    MAR-1812 added tests for TeamCards slice

---
+++ tests/prismicSlices/pageParts/TeamCards/index.spec.js
@@ -0,0 +1,52 @@
+import { render, screen } from '@testing-library/vue'
+import TeamCards from '@/prismicSlices/pageParts/TeamCards'
+
+const props = {
+  slice: {
+    primary: {
+      animation: 'fade-up',
+    },
+    items: [
+      {
+        name: 'Uncle Bob',
+        image: {
+          url: 'https://google.com/',
+          alt: 'Alt text',
+        },
+        position: 'Senior',
+        linkedin: {
+          url: 'https://linkedin.com/bob/',
+        },
+      },
+      {
+        name: 'Alice',
+        image: {
+          url: 'https://yandex.ru/',
+          alt: 'Text Alt',
+        },
+        position: 'Team Lead',
+        linkedin: {
+          url: 'https://linkedin.com/alice/',
+        },
+      },
+    ],
+  },
+}
+
+describe('TeamCards slice', () => {
+  it('should render correctly', () => {
+    const { container } = render(TeamCards, {
+      props,
+    })
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should render correctly with data', () => {
+    render(TeamCards, {
+      props,
+    })
+
+    expect(screen.getByText('Uncle Bob')).not.toBeNull()
+    expect(screen.getByText('Alice')).not.toBeNull()
+  })
+})
