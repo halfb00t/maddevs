a61712cc259e553735f7ff53c0bbb7096c2dccfe
Kirill kotyunin <kotyunin.k@gmail.com>
2021-09-08 17:02:25 +0600

    MAR-1908 Added unit-tests for the custom page store

--- client/store/modules/customPage.js
+++ client/store/modules/customPage.js
@@ -3,7 +3,7 @@ import { getCustomPage } from '@/api/customPage'
 import extractSchemaOrg from '@/helpers/extractSchemaOrg'
 
 const extractCustomPageData = page => {
-  if (!page) return {}
+  if (!page && !page.data) return {}
   return {
     released: page.data.released,
     showFooter: page.data.show_footer,
