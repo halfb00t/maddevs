6cd91c4d40bbb6bb82e5d49f09fb20818c77354b
Ignat Kim <kimignat@gmail.com>
2021-09-10 19:34:42 +0600

    MAR-1940 Added Prototype.vue section and test

--- client/components/Cases/citycam/Main.vue
+++ client/components/Cases/citycam/Main.vue
@@ -3,6 +3,7 @@
     <About />
     <WhyCityCam />
     <ChallengesAndSolutions />
+    <Prototype />
   </section>
 </template>
 
@@ -11,10 +12,12 @@
 import About from '@/components/Cases/citycam/About'
 import WhyCityCam from '@/components/Cases/citycam/WhyCityCam'
 import ChallengesAndSolutions from '@/components/Cases/citycam/ChallengesAndSolutions'
+import Prototype from '@/components/Cases/citycam/Prototype'
 
 export default {
   name: 'Main',
   components: {
+    Prototype,
     ChallengesAndSolutions,
     About,
     WhyCityCam,
---
+++ tests/components/Cases/citycam/Prototype.spec.js
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/vue'
+import Prototype from '@/components/Cases/citycam/Prototype'
+
+const stubs = ['Lottie']
+
+const mocks = {
+  $getMediaFromS3: () => 'img.jpg',
+}
+
+describe('Prototype component', () => {
+  it('should render correctly', () => {
+    const { container } = render(Prototype, {
+      stubs,
+      mocks,
+    })
+
+    expect(container).toMatchSnapshot()
+  })
+})
