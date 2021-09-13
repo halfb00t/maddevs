544dd4a0bd1d08a9851ece041ae259f3b16c406a
Ignat Kim <kimignat@gmail.com>
2021-09-13 18:06:27 +0600

    MAR-2001 Added Technologies.vue and its test

--- client/components/Cases/citycam/Main.vue
+++ client/components/Cases/citycam/Main.vue
@@ -7,6 +7,7 @@
     <PowerSupply />
     <DevicePerfecting />
     <OngoingProgress />
+    <Technologies />
   </section>
 </template>
 
@@ -19,10 +20,12 @@ import Prototype from '@/components/Cases/citycam/Prototype'
 import PowerSupply from '@/components/Cases/citycam/PowerSupply'
 import DevicePerfecting from '@/components/Cases/citycam/DevicePerfecting'
 import OngoingProgress from '@/components/Cases/citycam/OngoingProgress'
+import Technologies from '@/components/Cases/citycam/Technologies'
 
 export default {
   name: 'Main',
   components: {
+    Technologies,
     OngoingProgress,
     DevicePerfecting,
     PowerSupply,
--- client/data/caseCityCam.js
+++ client/data/caseCityCam.js
@@ -122,3 +122,106 @@ export const sliderTwo = [
     alt: 'Ongoing progress',
   },
 ]
+
+export const technologies = [
+  {
+    name: 'python',
+    alt: 'Python',
+    imgAlt: 'Python',
+  },
+  {
+    name: 'django',
+    alt: 'Django',
+    imgAlt: 'Django',
+  },
+  {
+    name: 'minio',
+    alt: 'MinIO',
+    imgAlt: 'MinIO',
+  },
+  {
+    name: 'go',
+    alt: 'Go',
+    imgAlt: 'Go',
+  },
+  {
+    name: 'postgresql',
+    alt: 'PostgreSQL',
+    imgAlt: 'PostgreSQL',
+  },
+  {
+    name: 'react',
+    alt: 'React',
+    imgAlt: 'React',
+  },
+  {
+    name: 'celery',
+    alt: 'Celery',
+    imgAlt: 'Celery',
+  },
+  {
+    name: 'prometheus',
+    alt: 'Prometheus',
+    imgAlt: 'Prometheus',
+  },
+  {
+    name: 'grafana',
+    alt: 'Grafana',
+    imgAlt: 'Grafana',
+  },
+  {
+    name: 'c',
+    alt: 'C',
+    imgAlt: 'C',
+  },
+  {
+    name: 'st-microelectronics',
+    alt: 'STMicroelectronics',
+    imgAlt: 'STMicroelectronics',
+  },
+  {
+    name: 'texas-instruments',
+    alt: 'Texas Instruments',
+    imgAlt: 'Texas Instruments',
+  },
+  {
+    name: 'arm',
+    alt: 'ARM',
+    imgAlt: 'ARM',
+  },
+  {
+    name: 'easy-eda',
+    alt: 'EasyEDA',
+    imgAlt: 'EasyEDA',
+  },
+  {
+    name: 'jlcpcb',
+    alt: 'JLCPCB',
+    imgAlt: 'JLCPCB',
+  },
+  {
+    name: 'webench-power-designer',
+    alt: 'WEBANCH Power Designer',
+    imgAlt: 'WEBANCH Power Designer',
+  },
+  {
+    name: 'corel-draw',
+    alt: 'Corel Draw',
+    imgAlt: 'Corel Draw',
+  },
+  {
+    name: 'topor',
+    alt: 'Topor',
+    imgAlt: 'Topor',
+  },
+  {
+    name: 'solidworks',
+    alt: 'SOLIDWORKS',
+    imgAlt: 'SOLIDWORKS',
+  },
+  {
+    name: 'altium-designer',
+    alt: 'Altium Designer',
+    imgAlt: 'Altium Designer',
+  },
+]
