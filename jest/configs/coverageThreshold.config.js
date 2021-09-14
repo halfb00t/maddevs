8346d869ee0b5e9a2562d3cf3800d4ad6a2fd2a9
Kirill kotyunin <kotyunin.k@gmail.com>
2021-09-14 11:43:46 +0600

    MAR-2022 Changed coverage threshold

--- jest/configs/coverageThreshold.config.js
+++ jest/configs/coverageThreshold.config.js
@@ -2,9 +2,9 @@ module.exports = {
   check: {
     global: {
       lines: 91,
-      statements: 90.9,
-      branches: 78.8,
-      functions: 85.9,
+      statements: 90.5,
+      branches: 78.5,
+      functions: 85.5,
     },
   },
 }
