9c9f6559db260d48ee9a8eaed07b2f01cd5a3591
TangoPJ <rail@maddevs.io>
2021-09-15 06:03:54 +0000

    MAR-1617. Added styles to index component

--- client/prismicSlices/pageParts/TechnologiesAndTools/index.vue
+++ client/prismicSlices/pageParts/TechnologiesAndTools/index.vue
@@ -1,13 +1,20 @@
 <template>
-  <section>
+  <section
+    class="card-grid-main"
+    :style="{ backgroundColor: sliceBackground }"
+  >
     <TechnologiesAndTools
       v-if="slice.variation === 'default-slice'"
       v-bind="slice"
       :data-aos="slice.primary.animation"
+      data-testid="technologies-grid-variation"
     />
     <ToolsGrid
       v-if="slice.variation !== 'default-slice'"
       v-bind="slice.primary"
+      :data-aos="slice.primary.animation"
+      :items="slice.items"
+      data-testid="tools-grid-variation"
     />
   </section>
 </template>
@@ -41,6 +48,15 @@ export default {
       default() {
         return {}
       },
+    },
+  },
+
+  computed: {
+    sliceBackground() {
+      if (this.slice?.primary?.background === 'white') return '#ffffff'
+      if (this.slice?.primary?.background === 'grey') return '#f5f7f9'
+      if (this.slice?.primary?.background === 'black') return '#111213'
+      return null
     },
   },
 }
