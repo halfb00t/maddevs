5d4e4dae33577509a95ad51e4a03012cbd642b64
TangoPJ <rail@maddevs.io>
2021-09-15 16:10:24 +0000

    MAR-1617-new. Updated branch

---
+++ client/prismicSlices/pageParts/TechnologiesAndTools/variations/ToolsGrid.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="tool-slice">
+    <div
+      class="container"
+      :data-aos="animation"
+    >
+      Hello
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ToolsGrid',
+}
+</script>
