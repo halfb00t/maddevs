ba8721c3eef66061d5b1243275a3dca01031f34f
denisoed <denisoed@gmail.com>
2021-09-16 14:36:53 +0600

    MAR-865 updated logic for TextSlice component

--- client/components/slices/DoubleColumnBorderedSlice.vue
+++ client/components/slices/DoubleColumnBorderedSlice.vue
@@ -29,28 +29,61 @@ export default {
   border: 1px solid $border-color--silver;
   margin-bottom: 48px;
   margin-top: 24px;
-  padding: 44px 24px 24px;
+  padding: 24px;
 
   &__inner {
     display: grid;
     grid-template-columns: 3fr 1fr;
     grid-gap: 20px;
+
     @media screen and (max-width: 991px) {
       grid-template-columns: 1fr;
     }
+
     & > div {
       margin-top: 0;
     }
+
+    /deep/ h1 {
+      font-size: 21px;
+      line-height: 28px;
+      margin: 0;
+    }
+
+    /deep/ h2 {
+      font-size: 21px;
+      line-height: 28px;
+      margin: 0;
+    }
+
+    /deep/ h3 {
+      font-size: 21px;
+      line-height: 28px;
+      margin: 0;
+    }
+
+    /deep/ h4 {
+      font-size: 21px;
+      line-height: 28px;
+      margin: 0;
+    }
+
     /deep/ h5 {
       font-size: 21px;
       line-height: 28px;
-      margin-bottom: 6px;
+      margin: 0;
     }
+
+    /deep/ h6 {
+      font-size: 21px;
+      line-height: 28px;
+      margin: 0;
+    }
+
     /deep/ p {
-      font-size: 13px;
-      line-height: 22px;
-      margin-bottom: 6px;
-      color: $text-color--grey-pale;
+      font-size: 21px;
+      line-height: 28px;
+      margin: 0;
     }
   }
 
