dbb10b8174c5f02ba9fad745e3176e2c3ecadddc
TangoPJ <rail@maddevs.io>
2021-09-21 11:01:06 +0000

    MAR-1617-new. Fixed problems

---
+++ client/prismicSlices/pageParts/CardGridSlice/components/CardAnimation.vue
@@ -0,0 +1,35 @@
+<template>
+  <Lottie
+    id="dev-page"
+    :options="options"
+    @animCreated="handleAnimation"
+  />
+</template>
+
+<script>
+import Lottie from 'vue-lottie/src/lottie.vue'
+import playLottieMixin from '@/mixins/playLottieMixin'
+
+export default {
+  name: 'CardAnimation',
+
+  components: {
+    Lottie,
+  },
+
+  mixins: [
+    playLottieMixin('dev-page', {
+      animationData: () => require(`@/assets/lottie/development/${this.animation}.json`),
+      autoplay: false,
+      loop: false,
+    }),
+  ],
+
+  props: {
+    animation: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
--- client/prismicSlices/pageParts/CardGridSlice/variations/RichTextCardsAnimation.vue
+++ client/prismicSlices/pageParts/CardGridSlice/variations/RichTextCardsAnimation.vue
@@ -1,29 +1,171 @@
 <template>
-  <section>
-    <div class="lottie">
-      pass
+  <div class="rich-text-cards">
+    <div class="container">
+      <ul class="rich-text-cards__grid">
+        <li
+          v-for="item in items"
+          :key="item.title"
+          :class="[
+            item.fullWidth ? 'card-item--full-width' : null,
+            item.alignText ? `card-item--text-${item.alignText}` : null,
+          ]"
+          :style="{ backgroundColor: item.background || null }"
+          class="card-item"
+          data-testid="rich-text-animation-item"
+        >
+          <div class="card-item__lottie">
+            <CardAnimation
+              :animation="item['lottie-animations']"
+            />
+          </div>
+          <h3
+            class="card-item__title"
+            v-html="item.title"
+          />
+          <div
+            class="card-item__content"
+            v-html="renderCardContent($prismic.asHtml(item.content))"
+          />
+        </li>
+      </ul>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
-// import Lottie from 'vue-lottie/src/lottie.vue'
-import playLottieMixin from '@/mixins/playLottieMixin'
-import animationData from '@/assets/lottie/development/lottie_anim2.json'
+import CardAnimation from '@/prismicSlices/pageParts/CardGridSlice/components/CardAnimation'
 
 export default {
   name: 'RichTextCardsAnimation',
 
-  // components: {
-  //   Lottie,
-  // },
+  components: {
+    CardAnimation,
+  },
 
-  mixins: [
-    playLottieMixin('power-supply', {
-      animationData,
-      autoplay: false,
-      loop: false,
-    }),
-  ],
+  props: {
+    items: {
+      type: Array,
+      default: () => ([]),
+    },
+  },
+
+  methods: {
+    renderCardContent(content) {
+      if (typeof content !== 'string') return ''
+      return content
+        .replace(/&lt;/g, '<')
+        .replace(/&gt;/g, '>')
+        .replace(/&quot;/g, '"')
+    },
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+.rich-text-cards {
+  &__grid {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 30px;
+    @media screen and (max-width: 1260px) {
+      grid-gap: 24px;
+    }
+    @media screen and (max-width: 1024px) {
+      grid-template-columns: 1fr;
+    }
+  }
+}
+
+.card-item {
+  display: block;
+  box-sizing: border-box;
+  padding: 80px;
+  padding-bottom: 96px;
+  color: $text-color--white-primary;
+  background-color: $bgcolor--black-pale;
+  @media screen and (max-width: 1024px) {
+    padding: 32px;
+  }
+
+  &--full-width {
+    grid-column: auto/span 2;
+    @media screen and (max-width: 1024px) {
+      grid-column: auto;
+    }
+  }
+
+  &__lottie {
+    max-width: 385px;
+    height: 95px;
+    margin-bottom: 60px;
+  }
+
+  &--text-center {
+    .card-item__title {
+      text-align: center;
+      @media screen and (max-width: 1024px) {
+        text-align: left;
+      }
+    }
+    /deep/ p {
+      text-align: center;
+      @media screen and (max-width: 1024px) {
+        text-align: left;
+      }
+    }
+  }
+
+  &__title {
+    @include font('Inter', 56px, 800);
+    line-height: 65px;
+    letter-spacing: -0.04em;
+    max-width: 1080px;
+    @media screen and (max-width: 1260px) {
+      font-size: 30px;
+      line-height: 37px;
+    }
+  }
+
+  &__content {
+    @include font('Inter', 24px, 600);
+    line-height: 38px;
+    letter-spacing: -0.013em;
+    word-break: break-word;
+    max-width: 1080px;
+    @media screen and (max-width: 1260px) {
+      font-size: 20px;
+      line-height: 30px;
+    }
+
+    /deep/ p {
+      margin-top: 40px;
+    }
+
+    /deep/ li {
+      font-weight: 400;
+      margin-top: 7px;
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+
+    /deep/ ul {
+      li {
+        &:before {
+          content: '';
+          display: inline-block;
+          width: 20px;
+          min-width: 20px;
+          height: 1.5px;
+          background-color: $border-color--red;
+          vertical-align: top;
+          margin: 18px 12px 0 0;
+          @media screen and (max-width: 1260px) {
+            margin-top: 14px;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
