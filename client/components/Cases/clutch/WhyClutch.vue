3b868ffc60a45d83b49a56083aa39d1a730153ee
Ignat Kim <kimignat@gmail.com>
2021-09-23 16:38:26 +0600

    MAR-2093 prepared WhyClutch.vue section

---
+++ client/components/Cases/clutch/WhyClutch.vue
@@ -0,0 +1,81 @@
+<template>
+  <section>
+    <div class="container_regular">
+      <h2 class="case_title_h2 m-12_bottom">
+        Why do top firms & decision <br>
+        makers trust Clutch?
+      </h2>
+      <TextParagraph class="m-24_bottom media-m-12_bottom">
+        Clutch's founder created the platform to make it easy to perform credible and accessible market research. They
+        saw a challenge in how advertorials and superficial data distorted businesses’ buying decisions.
+      </TextParagraph>
+      <div
+        class="p-96_bottom media-p-48_bottom"
+      >
+        <ListNumberedBox>
+          <ListNumberedItemBox
+            v-for="(solution, i) in whyClutch"
+            :id="`whyClutch-item-${i}`"
+            :key="solution.text"
+            :data-background="solution.background"
+          >
+            {{ solution.text }}
+          </ListNumberedItemBox>
+        </ListNumberedBox>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import TextParagraph from '@/components/Cases/shared/TextParagraph'
+import ListNumberedBox from '@/components/Cases/shared/ListNumberedBox'
+import ListNumberedItemBox from '@/components/Cases/shared/ListNumberedItemBox'
+import IntersectionObserverMixin from '@/mixins/IntersectionObserverMixin'
+import setBackground from '@/helpers/setBackground'
+import { idWhyClutch, whyClutch } from '@/data/clutchCase' // file will be updated to the one in MAR-2092 after merge.
+
+export default {
+  name: 'WhyClutch',
+  components: {
+    TextParagraph,
+    ListNumberedBox,
+    ListNumberedItemBox,
+  },
+
+  mixins: [
+    IntersectionObserverMixin(
+      idWhyClutch,
+      setBackground,
+    ),
+  ],
+
+  data() {
+    return {
+      whyClutch,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.case {
+  &_lottie {
+    max-width: 813px;
+  }
+  &_title_h2 br{
+    @media screen and (max-width: 390px) {
+      display: none;
+    }
+  }
+
+  /deep/ &_list-numbered_item {
+    color: $text-color--white-primary;
+    background-color: $bgcolor--grey-cases;
+    transition: 1s;
+    @media screen and (max-width: 768px) {
+      background-color: $bgcolor--grey-cases;
+    }
+  }
+}
+</style>
---
+++ client/data/clutchCase.js
@@ -0,0 +1,20 @@
+export const whyClutch = [
+  {
+    text: 'Clutch is a credible platform with data, case studies, and client testimonials that guide readers through the process of choosing a business service or solution.',
+    background: '#22B3E2',
+  },
+  {
+    text: 'Clutch provides a dedicated team that interviews real clients, collects data, and enables comparison against competitors to help users find suitable partners.',
+    background: '#1B85CE',
+  },
+  {
+    text: 'The clear indication of paid endorsements and sponsored posts on Clutch enables well-informed market research.',
+    background: '#043AB4',
+  },
+]
+
+export const idWhyClutch = [
+  'whyClutch-item-0',
+  'whyClutch-item-1',
+  'whyClutch-item-2',
+]
