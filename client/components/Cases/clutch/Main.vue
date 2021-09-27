43984dcf0590c46a938a0b56adfef335649fc30b
Ignat Kim <kimignat@gmail.com>
2021-09-27 20:46:01 +0600

    MAR-2098 - added WidgetSpeed.vue section and its test

--- client/components/Cases/clutch/Main.vue
+++ client/components/Cases/clutch/Main.vue
@@ -1,16 +1,25 @@
 <template>
   <section>
+    <About />
+    <WhyClutch />
     <TechChallenges />
+    <WidgetSpeed />
   </section>
 </template>
 
 <script>
 
 import TechChallenges from '@/components/Cases/clutch/TechChallenges'
+import WidgetSpeed from '@/components/Cases/clutch/WidgetSpeed'
+import About from '@/components/Cases/clutch/About'
+import WhyClutch from '@/components/Cases/clutch/WhyClutch'
 
 export default {
   name: 'Main',
   components: {
+    WhyClutch,
+    About,
+    WidgetSpeed,
     TechChallenges,
   },
 }
