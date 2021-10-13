6682fb8db1711840372a013749151b704666b26e
Ignat Kim <kimignat@gmail.com>
2021-10-13 17:11:26 +0600

    MAR-2268 Filtering of incoming requests on Contact Me form

--- server/controllers/LeadsController.js
+++ server/controllers/LeadsController.js
@@ -1,10 +1,14 @@
-const { sendMailFromVariables } = require('../services/EmailsService')
+const { sendMailFromVariables, sendEmailToRequest, verifyEmailDelivery } = require('../services/EmailsService')
 const { createLead } = require('../services/LeadsService')
 const { validate } = require('../utils/validation')
 
 async function create(req, res) {
   const { isValid, error } = validate(req, 'email')
   if (!isValid) return res.status(error.status).json(error)
+
+  const delivery = await sendEmailToRequest(req.body)
+  const deliveryReport = await verifyEmailDelivery(delivery.id)
+  if (deliveryReport.smtp_answer_code === 550 && req.body.variables.phoneNumber === '') return res.json({ error: deliveryReport?.smtp_answer_code })
 
   await sendMailFromVariables(req.body)
   const response = await createLead(req)
--- server/services/EmailsService.js
+++ server/services/EmailsService.js
@@ -69,6 +69,29 @@ async function sendMailFromVariables({ variables, templateId, attachment }) {
   return send(email)
 }
 
+async function verifyEmailDelivery(id) {
+  return new Promise(resolve => {
+    sendpulse.smtpGetEmailInfoById(data => {
+      resolve(data)
+    }, id)
+  })
+}
+
+async function sendEmailToRequest({ variables }) {
+  const emailParams = {
+    templateId: 672221, // Required
+    variables,
+    subject: variables.subject,
+    fromName: 'Mad Devs Team',
+    fromEmail: DEFAULT_FROM_EMAIL,
+    toName: variables.fullName,
+    toEmail: variables.email,
+  }
+
+  const email = buildEmail(emailParams)
+  return send(email)
+}
+
 async function sendCVResponseMail({ variables }) {
   const emailParams = {
     templateId: 638666,
@@ -80,11 +103,13 @@ async function sendCVResponseMail({ variables }) {
     toEmail: variables.email,
   }
 
-  const email = await buildEmail(emailParams)
+  const email = buildEmail(emailParams)
   return send(email)
 }
 
 module.exports = {
   sendMailFromVariables,
+  sendEmailToRequest,
   sendCVResponseMail,
+  verifyEmailDelivery,
 }
