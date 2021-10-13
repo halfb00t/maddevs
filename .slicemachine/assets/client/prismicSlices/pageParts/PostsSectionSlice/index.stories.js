60630ff36f789e7641aa9db0dd7eb1529dabe4ee
TangoPJ <livechess@yahoo.com>
2021-10-13 16:26:24 +0000

    MAR-1933. Fixed problems

--- .slicemachine/assets/client/prismicSlices/pageParts/PostsSectionSlice/index.stories.js
+++ .slicemachine/assets/client/prismicSlices/pageParts/PostsSectionSlice/index.stories.js
@@ -18,7 +18,7 @@ export const _DefaultSlice = () => ({
   },
   data() {
     return {
-      mock: {"variation":"default-slice","name":"Default slice","slice_type":"posts_section_slice","items":[{"theme":"light","uid":"utilize ubiquitous web-readiness","type":"post","url":"repurpose best-of-breed ROI","dimensionWidth":"maximize next-generation markets","dimensionHeight":"integrate collaborative e-business","alt":"extend web-enabled mindshare","date":"2013-10-14"},{"theme":"light","uid":"repurpose B2C e-business","type":"tag","url":"scale e-business partnerships","dimensionWidth":"deploy customized synergies","dimensionHeight":"brand frictionless convergence","alt":"aggregate cross-media niches","date":"2017-05-19"},{"theme":"dark","uid":"seize synergistic blockchains","type":"tag","url":"unleash vertical technologies","dimensionWidth":"optimize customized partnerships","dimensionHeight":"strategize scalable relationships","alt":"seize bleeding-edge bandwidth","date":"2018-10-13"}],"primary":{"animation":"fade-up"},"id":"_DefaultSlice"}
+      mock: {"variation":"default-slice","name":"Default slice","slice_type":"posts_section_slice","items":[{"theme":"light","data":{"link_type":"Web","url":"http://twitter.com"}},{"theme":"light","data":{"link_type":"Web","url":"https://slicemachine.dev"}},{"theme":"light","data":{"link_type":"Web","url":"http://google.com"}},{"theme":"dark","data":{"link_type":"Web","url":"https://slicemachine.dev"}},{"theme":"light","data":{"link_type":"Web","url":"http://google.com"}}],"primary":{"animation":"fade-up"},"id":"_DefaultSlice"}
     }
   },
   template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
