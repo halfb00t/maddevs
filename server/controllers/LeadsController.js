42fd4e3f9584fe02a1246574a3f5d2a44411d30f
Ignat Kim <kimignat@gmail.com>
2021-10-13 17:55:07 +0600

    MAR-2268 closed review notes

--- server/controllers/LeadsController.js
+++ server/controllers/LeadsController.js
@@ -2,13 +2,15 @@ const { sendMailFromVariables, sendEmailToRequest, verifyEmailDelivery } = requi
 const { createLead } = require('../services/LeadsService')
 const { validate } = require('../utils/validation')
 
+const ADDRESS_NOT_FOUND_ERROR = 550
+
 async function create(req, res) {
   const { isValid, error } = validate(req, 'email')
   if (!isValid) return res.status(error.status).json(error)
 
   const delivery = await sendEmailToRequest(req.body)
   const deliveryReport = await verifyEmailDelivery(delivery.id)
-  if (deliveryReport.smtp_answer_code === 550 && req.body.variables.phoneNumber === '') return res.json({ error: deliveryReport?.smtp_answer_code })
+  if (deliveryReport.smtp_answer_code === ADDRESS_NOT_FOUND_ERROR && !req.body.variables.phoneNumber) return res.json({ error: deliveryReport?.smtp_answer_code })
 
   await sendMailFromVariables(req.body)
   const response = await createLead(req)
