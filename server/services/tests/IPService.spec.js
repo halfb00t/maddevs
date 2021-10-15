fcd2f5831993bd67e9bc65eb61fb03320421009c
Ernest2077 <ernest1637@gmail.com>
2021-10-15 13:33:59 +0600

    MAR-2292 Added test for getLocationByIP

--- server/services/tests/IPService.spec.js
+++ server/services/tests/IPService.spec.js
@@ -1,14 +1,20 @@
 import 'regenerator-runtime'
+import fetch from 'node-fetch'
 import * as IPService from '../IPService'
 
-jest.mock('node-fetch', () => jest.fn(() => Promise.resolve({
-  country: 'Kyrgyzstan',
-  city: 'Bishkek',
-})))
+jest.mock('node-fetch', () => (
+  {
+    default: jest.fn(),
+  }
+))
 
 jest.mock('../../config/env', () => ({
   IP_BAN_LIST: '212.42.107.138, 212.42.107.133',
 }))
+
+const getLocationData = locationData => Promise.resolve({
+  json: jest.fn(() => Promise.resolve(locationData)),
+})
 
 describe('IPService', () => {
   it('should correctly return IP by request', async () => {
@@ -28,5 +34,29 @@ describe('IPService', () => {
     const isBlocked = IPService.isBlockedIP(IP)
     expect(isBlocked)
       .toBeTruthy()
+  })
+
+  it('should correctly return Location by IP', async () => {
+    fetch.default.mockImplementation(() => getLocationData({
+      city: 'bishkek',
+      country: 'Kyrgyzstan',
+    }))
+
+    const location = await IPService.getLocationByIP()
+    expect(location)
+      .toEqual({
+        city: 'bishkek',
+        country: 'Kyrgyzstan',
+      })
+  })
+  it('should correctly return Location by IP (alternative case)', async () => {
+    fetch.default.mockImplementation(() => getLocationData({}))
+
+    const location = await IPService.getLocationByIP()
+    expect(location)
+      .toEqual({
+        city: '-',
+        country: '-',
+      })
   })
 })
