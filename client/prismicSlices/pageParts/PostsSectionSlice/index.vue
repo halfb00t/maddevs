003eb387a8692cd089c70a4631a92ab03812766d
TangoPJ <livechess@yahoo.com>
2021-10-19 08:53:18 +0000

    MAR-1933. Created functions to get info from prismic && updated snapshots

--- client/prismicSlices/pageParts/PostsSectionSlice/index.vue
+++ client/prismicSlices/pageParts/PostsSectionSlice/index.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="tag-posts">
-    <div class="container">
+    <div
+      :data-aos="animation"
+      class="container"
+    >
       <div class="tag-posts__list">
-        <template v-if="slice.items">
+        <template v-if="posts">
           <section
-            v-for="post in slice.items"
-            :key="post.data.id"
+            v-for="post in posts"
+            :key="post.id"
             :post="post"
             class="tag-posts__list-item"
           >
             <PostCard
               :post="post"
-              :tag="blogTag"
-              :author="findAuthor(post.data.id, allAuthors)"
-              :disable-tag-link="true"
+              :tag="post.tags[0]"
+              :author="findAuthor(post.data.post_author.id, authors)"
+              :disable-tag-link="false"
             />
           </section>
         </template>
         <template v-else>
           <section
-            v-for="i in 6"
+            v-for="i in slice.items"
             :key="i"
             class="tag-posts__list-item"
           >
@@ -34,6 +37,8 @@
 <script>
 import SkeletonBlogWidget from '@/components/Blog/skeletons/SkeletonBlogWidget'
 import PostCard from '@/components/Blog/shared/PostCard'
+import findPostAuthorMixin from '@/mixins/findPostAuthorMixin'
+import extractAuthorData from '@/helpers/extractAuthorData'
 
 export default {
   name: 'PostsSectionSlice',
@@ -41,6 +46,8 @@ export default {
     SkeletonBlogWidget,
     PostCard,
   },
+
+  mixins: [findPostAuthorMixin],
 
   props: {
     slice: {
@@ -52,13 +59,33 @@ export default {
     },
   },
 
-  mounted() {
-    this.showData()
+  data() {
+    return {
+      animation: this.slice.primary.animation,
+      posts: [],
+      authors: [],
+    }
+  },
+
+  watch: {
+    async posts() {
+      if (this.posts && this.posts.length) {
+        const authorIDs = this.posts.map(result => result.data.post_author.id)
+        const postsAuthors = await this.getPrismicData(authorIDs)
+        this.authors = postsAuthors.map(author => extractAuthorData(author))
+      }
+    },
+  },
+
+  async mounted() {
+    const postIDs = this.slice.items?.map(item => item.data.id)
+    if (postIDs && postIDs.length) this.posts = await this.getPrismicData(postIDs)
   },
 
   methods: {
-    showData() {
-      console.log(this.slice.items[0].data.id)
+    async getPrismicData(ids) {
+      const response = await this.$prismic.api.getByIDs(ids)
+      return response.results
     },
   },
 }
@@ -80,9 +107,6 @@ export default {
       width: 33.3333%;
       padding: 0 10px;
       margin-bottom: 103px;
-      &:first-of-type {
-        display: none;
-      }
       @media only screen and (min-width: 991px) {
         &:nth-last-child(-n+3) {
           margin-bottom: 0;
@@ -96,9 +120,6 @@ export default {
       &__list-item {
         width: 100%;
         margin-bottom: 56px;
-        &:first-of-type {
-          display: block;
-        }
         &:last-child {
           margin-bottom: 0;
         }
