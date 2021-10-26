c021ac10a7e21eec6aa8f9d0fdc2b9d2c02c0aad
Ignat Kim <kimignat@gmail.com>
2021-10-26 22:07:34 +0600

    MAR-2283 modified CareersController.test.js test unit test

--- server/controllers/CareersController.js
+++ server/controllers/CareersController.js
@@ -2,6 +2,7 @@ const { sendMailFromVariables, sendCVResponseMail } = require('../services/Email
 const { sendApplication } = require('../services/HuntflowService')
 const { validate } = require('../utils/validation')
 const { getIPByRequest, getLocationByIP, isBlockedIP } = require('../services/IPService')
+const { reCaptchaVerification } = require('../services/reCaptchaVerification')
 
 const parseRequest = req => ({
   ...req,
@@ -16,11 +17,16 @@ const buildRequest = (req, key) => ({
 })
 
 async function index(req, res) {
+  const parsedReq = parseRequest(req)
+  if (!parsedReq.body.token) return res.json({ success: false, message: 'Invalid token' })
+
+  const { data } = await reCaptchaVerification(parsedReq.body.token)
+  if (!data?.success) return res.json({ success: data.success, message: data['error-codes'] })
+
   const ip = getIPByRequest(req)
   const { city, country } = await getLocationByIP(ip)
   if (isBlockedIP(ip)) return res.json({ error: 'Your ip is in a blacklist' })
 
-  const parsedReq = parseRequest(req)
   const huntflowReq = buildRequest(parsedReq, 'huntflow')
   const emailReq = {
     ...buildRequest(parsedReq, 'email'),
