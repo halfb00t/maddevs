065f7c85ebc57bf4fdaab7d1f1b6c516d8d1bd95
TangoPJ <rail@maddevs.io>
2021-10-29 17:09:07 +0600

    MAR-2436. Fixed problems

--- .slicemachine/assets/client/prismicSlices/pageParts/ImageSlice/index.stories.js
+++ .slicemachine/assets/client/prismicSlices/pageParts/ImageSlice/index.stories.js
@@ -18,7 +18,7 @@ export const _DefaultSlice = () => ({
   },
   data() {
     return {
-      mock: {"variation":"default-slice","name":"Default slice","slice_type":"image_slice","items":[],"primary":{"image":{"dimensions":{"width":1240,"height":607},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1437622368342-7a3d73a34c8f?w=1240&h=607&fit=crop"},"colorTheme":"grey","animation":"fade-down-left"},"id":"_DefaultSlice"}
+      mock: {"variation":"default-slice","name":"Default slice","slice_type":"image_slice","items":[],"primary":{"image":{"dimensions":{"width":null,"height":null},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1437622368342-7a3d73a34c8f?w=null&h=null&fit=crop"},"colorTheme":"black","animation":"zoom-in"},"id":"_DefaultSlice"}
     }
   },
   template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
