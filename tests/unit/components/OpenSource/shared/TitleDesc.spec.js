b0102214c4444af9d19a42080a8d3027e7a1ee0c
Ernest2077 <ernest1637@gmail.com>
2021-11-02 18:03:31 +0600

    MAR-2343 Added new test

---
+++ tests/unit/components/OpenSource/shared/TitleDesc.spec.js
@@ -0,0 +1,33 @@
+import { shallowMount } from '@vue/test-utils'
+import TitleDesc from '@/components/OpenSource/shared/TitleDesc'
+
+describe('TitleDesc component', () => {
+  it('should render correctly', () => {
+    const wrapper = shallowMount(TitleDesc, {
+      propsData: {
+        title: 'Test title',
+        description: 'Test description',
+        textColor: 'white',
+      },
+    })
+
+    const sectionClassName = wrapper.find('section').classes()[1]
+    const title = wrapper.find('h2').text()
+    const description = wrapper.find('p').text()
+
+    expect(sectionClassName).toBe('title-desc--white-text')
+    expect(title).toBe('Test title')
+    expect(description).toBe('Test description')
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render correctly without data', () => {
+    const wrapper = shallowMount(TitleDesc)
+
+    const sectionClassName = wrapper.find('section').classes()[1]
+    const title = wrapper.find('h2').text()
+
+    expect(sectionClassName).toBe('title-desc--black-text')
+    expect(title).toBe('')
+  })
+})
