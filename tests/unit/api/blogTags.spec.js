6cf04dda8f207bb98575c3e5dfcf369f361acee0
TangoPJ <rail@maddevs.io>
2021-11-10 16:41:47 +0600

    MAR-2421. Fixed some problems

--- tests/unit/api/blogTags.spec.js
+++ tests/unit/api/blogTags.spec.js
@@ -1,71 +1,75 @@
 import 'regenerator-runtime'
 import * as api from '@/api/blogTags'
+import { getBlogPosts } from '@/api/blog'
 
-describe('BlogTags api methods', () => {
-  const error = new Error('error')
-  const failure = jest.fn(() => Promise.reject(error))
+jest.mock('@/api/blog', () => ({ getBlogPosts: jest.fn() }))
+
+describe('getBlogTags', () => {
+  const tags = ['tag1', 'tag2']
+  const response = [{ tags }]
+  getBlogPosts.mockImplementation(() => Promise.resolve(response))
+  const prismic = jest.fn()
+
+  it('success', async () => {
+    const data = await api.getBlogTags(prismic)
+    expect(data).toEqual(['tag1', 'tag2'])
+  })
+
+  it('failure', async () => {
+    const error = new Error('Test error')
+    getBlogPosts.mockImplementation(() => Promise.reject(error))
+    const result = await api.getBlogTags(prismic)
+    expect(result).toBe(error)
+  })
+})
+
+describe('getPostsByTag', () => {
+  const results = [{
+    data: {
+      post_author: {
+        id: '1',
+      },
+    },
+  }, {
+    data: {
+      post_author: {},
+    },
+  }]
+
+  const success = jest.fn(() => Promise.resolve({ data: 'data', results }))
 
   const prismic = {
     api: {
-      query: null,
+      query: success,
     },
     predicates: {
       at: jest.fn(),
     },
   }
 
-  const prismicFailure = {
-    api: {
-      query: failure,
-    },
-    predicates: {
-      at: jest.fn(),
-    },
-  }
+  it('success', async () => {
+    const payload = 'payload'
+    const data = await api.getPostsByTag(prismic, payload)
 
-  describe('getBlogTags', () => {
-    const success = jest.fn(() => Promise.resolve({ data: 'data', results: [{ tags: ['results'] }] }))
-    prismic.api.query = success
-
-    it('success', async () => {
-      const data = await api.getBlogTags(prismic)
-      expect(data).toEqual(['results'])
-    })
-
-    it('failure', async () => {
-      const data = await api.getBlogTags(prismicFailure)
-      expect(data).toEqual(error)
-    })
+    expect(data).toHaveLength(1)
+    expect(data[0].data?.post_author?.id).toBe('1')
   })
 
-  describe('getPostsByTag', () => {
-    const results = [{
-      data: {
-        post_author: {
-          id: '1',
-        },
+  it('failure', async () => {
+    const error = new Error('Test error')
+    const failure = jest.fn(() => Promise.reject(error))
+    const prismicFailure = {
+      api: {
+        getSingle: failure,
+        query: failure,
+        getByUID: failure,
       },
-    }, {
-      data: {
-        post_author: {},
+      predicates: {
+        at: jest.fn(),
       },
-    }]
-
-    const success = jest.fn(() => Promise.resolve({ data: 'data', results }))
-    prismic.api.query = success
-
-    it('success', async () => {
-      const payload = 'payload'
-      const data = await api.getPostsByTag(prismic, payload)
-
-      expect(data).toHaveLength(1)
-      expect(data[0].data?.post_author?.id).toBe('1')
-    })
-
-    it('failure', async () => {
-      const payload = 'payload'
-      const data = await api.getPostsByTag(prismicFailure, payload)
-      expect(data).toBe(error)
-    })
+    }
+    const payload = 'payload'
+    const data = await api.getPostsByTag(prismicFailure, payload)
+    expect(data).toBe(error)
   })
 })
