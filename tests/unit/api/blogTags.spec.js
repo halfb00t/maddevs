c3f5be6683b070c645ad38250c6152e05ea777d7
TangoPJ <rail@maddevs.io>
2021-11-10 14:27:07 +0600

    MAR-2421. Added test to getBlogTags function

--- tests/unit/api/blogTags.spec.js
+++ tests/unit/api/blogTags.spec.js
@@ -1,27 +1,13 @@
 import 'regenerator-runtime'
 import * as api from '@/api/blogTags'
 
-const results = [{
-  data: {
-    post_author: {
-      id: '1',
-    },
-  },
-}, {
-  data: {
-    post_author: {
-    },
-  },
-}]
-
 describe('BlogTags api methods', () => {
   const error = new Error('error')
-  const success = jest.fn(() => Promise.resolve({ data: 'data', results }))
   const failure = jest.fn(() => Promise.reject(error))
 
   const prismic = {
     api: {
-      query: success,
+      query: null,
     },
     predicates: {
       at: jest.fn(),
@@ -37,17 +23,49 @@ describe('BlogTags api methods', () => {
     },
   }
 
-  it('getPostsByTag success', async () => {
-    const payload = 'payload'
-    const data = await api.getPostsByTag(prismic, payload)
+  describe('getBlogTags', () => {
+    const success = jest.fn(() => Promise.resolve({ data: 'data', results: [{ tags: ['results'] }] }))
+    prismic.api.query = success
 
-    expect(data).toHaveLength(1)
-    expect(data[0].data?.post_author?.id).toBe('1')
+    it('success', async () => {
+      const data = await api.getBlogTags(prismic)
+      expect(data).toEqual(['results'])
+    })
+
+    it('failure', async () => {
+      const data = await api.getBlogTags(prismicFailure)
+      expect(data).toEqual(error)
+    })
   })
 
-  it('getPostsByTag failure', async () => {
-    const payload = 'payload'
-    const data = await api.getPostsByTag(prismicFailure, payload)
-    expect(data).toBe(error)
+  describe('getPostsByTag', () => {
+    const results = [{
+      data: {
+        post_author: {
+          id: '1',
+        },
+      },
+    }, {
+      data: {
+        post_author: {},
+      },
+    }]
+
+    const success = jest.fn(() => Promise.resolve({ data: 'data', results }))
+    prismic.api.query = success
+
+    it('success', async () => {
+      const payload = 'payload'
+      const data = await api.getPostsByTag(prismic, payload)
+
+      expect(data).toHaveLength(1)
+      expect(data[0].data?.post_author?.id).toBe('1')
+    })
+
+    it('failure', async () => {
+      const payload = 'payload'
+      const data = await api.getPostsByTag(prismicFailure, payload)
+      expect(data).toBe(error)
+    })
   })
 })
