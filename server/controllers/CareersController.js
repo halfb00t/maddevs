b5e9bbff135c28a98d5c98c01b88404f130ff756
TangoPJ <rail@maddevs.io>
2021-11-16 12:50:42 +0600

    MAR-2504. Fixed name of a variable and fixed tests & updated snapshots

--- server/controllers/CareersController.js
+++ server/controllers/CareersController.js
@@ -24,7 +24,7 @@ async function index(req, res) {
   const huntflowReq = buildRequest(parsedReq, 'huntflow')
   const rawBodyReq = buildRequest(parsedReq, 'email')
 
-  const bodyReq = {
+  const emailReq = {
     ...rawBodyReq,
     body: {
       ...rawBodyReq.body,
@@ -39,12 +39,12 @@ async function index(req, res) {
   const huntflowValidation = validate(huntflowReq, 'huntflow')
   if (!huntflowValidation.isValid) return res.status(huntflowValidation.error.status).json(huntflowValidation.error)
 
-  const emailValidation = validate(bodyReq, 'email')
+  const emailValidation = validate(emailReq, 'email')
   if (!emailValidation.isValid) return res.status(emailValidation.error.status).json(emailValidation.error)
 
   const huntflowResponse = await sendApplication(huntflowReq)
-  const hrEmailResponse = await sendMailFromVariables(bodyReq.body)
-  const userEmailResponse = await sendCVResponseMail(bodyReq.body)
+  const hrEmailResponse = await sendMailFromVariables(emailReq.body)
+  const userEmailResponse = await sendCVResponseMail(emailReq.body)
 
   return res.json({ email: hrEmailResponse, userEmail: userEmailResponse, huntflow: huntflowResponse })
 }
