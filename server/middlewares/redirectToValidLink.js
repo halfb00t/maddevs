b8fbc234feabcd1aea439b70846a28ceb7d8316a
oh-cactus <roman.zh@maddevs.io>
2021-12-29 14:49:16 +0200

    Enabled trailingSlash. New logic for redirectToValidLinks middleware. Updated test

--- server/middlewares/redirectToValidLink.js
+++ server/middlewares/redirectToValidLink.js
@@ -6,6 +6,18 @@ function redirectToValidLink(req, res, next) {
     if (!req.secure) link = `https://${req.headers.host}${req.url}`
   }
 
+  // Add trailing slash to the URL if it's missing
+  if (req.url.substr(-1) !== '/'
+    && req.method === 'GET'
+    && req.url !== '/sitemap.xml'
+  ) {
+    if (link) {
+      link = `${link}/`
+    } else {
+      link = `http://${req.headers.host}${req.url}/`
+    }
+  }
+
   if (link) res.redirect(301, link)
   else next()
 }
