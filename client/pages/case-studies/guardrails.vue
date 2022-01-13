b386f250452868bbcbbaa157fb7fb9b108425769
Antoneo333 <anton.zaichenko@maddevs.io>
2022-01-13 18:05:33 +0600

    MAR-66. edited guardrails page header

--- client/pages/case-studies/guardrails.vue
+++ client/pages/case-studies/guardrails.vue
@@ -2,27 +2,38 @@
   <main class="case case--guardrails">
     <CaseHeader
       :logo="logo"
-      project="clutch"
-      video-name="/videos/clutch-banner.mp4"
+      project="guardrails"
+      video-name="/videos/guardrails-banner.mp4"
       video-fallback-path="/images/Cases/clutch/png/clutch-banner.png"
     >
+      <h1
+        slot="title"
+        class="case_header-title"
+      >
+        Enhancing the GuardRails solution
+      </h1>
+      <p
+        slot="description"
+        class="case_header-description"
+      >
+        Mad Devs assisted GuardRails an application security platform <br> to advance their product and increase its business value.
+      </p>
     </CaseHeader>
+    <!--    <Main />-->
     <Footer
-      link="/case-studies/sir-john-monash-centre/"
-      class-name="sjmc"
+      link="/case-studies/clutch/"
+      class-name="clutch"
     >
       <img
         slot="icon"
         v-lazy-load
-        :data-src="require(`@/assets/img/Projects/svg/sjmc-black.svg`)"
-        width="195"
-        height="72"
-        alt="Better cities with CityCam"
-        class="case_logotype-citycam"
+        :data-src="require(`@/assets/img/Studies/svg/clutch-footer.svg`)"
+        width="312"
+        height="138"
+        alt="Clutch"
+        class="case_logotype-clutch"
       >
-      The maintenance of memory:
-      <br>
-      Sir John Monash Centre
+      The growth of Clutch.co
     </Footer>
   </main>
 </template>
@@ -30,6 +41,7 @@
 <script>
 import CaseHeader from '@/components/Cases/shared/CaseHeader'
 import Footer from '@/components/Cases/shared/CaseFooter'
+// import Main from '@/components/Cases/clutch/Main'
 import { getMetadata, buildHead } from '@/data/seo'
 import animateOnScrollMixin from '@/mixins/animateOnScrollMixin'
 import headerMixin from '@/mixins/headerMixin'
@@ -38,7 +50,8 @@ export default {
   name: 'Clutch',
   components: {
     CaseHeader,
-    Footer,
+    Footer, //    Main,
+
   },
 
   nuxtI18n: false,
@@ -58,10 +71,10 @@ export default {
     return {
       logo: {
         width: 218,
-        height: 63,
-        folder: 'clutch',
-        file: 'clutch-logo',
-        alt: 'A platform of client reviews and content',
+        height: 300,
+        folder: 'guardrails',
+        file: 'guardrails-logo',
+        alt: 'GUARDRAILS',
       },
     }
   },
@@ -81,8 +94,8 @@ export default {
 @import '../../assets/styles/cases/media';
 
 .case {
-  &_header-clutch {
-    background-color: rgba(8, 83, 126, 0.9);
+  &_header-guardrails {
+    background-color:  #323A45;
   }
 
   &_header {
@@ -99,19 +112,19 @@ export default {
     }
   }
 
-  &_clutch-logo {
-    width: 218px;
-    height: 63px;
-
-    @media screen and (max-width: 1170px) {
-      margin-bottom: 50px;
-    }
-
-    @media screen and (max-width: 655px) {
-      width: 185px;
-      height: 53px;
-    }
-  }
+  //&_guardrails-logo {
+  //  //width: 218px;
+  //  //height: 300px;
+  //
+  //  @media screen and (max-width: 1170px) {
+  //    margin-bottom: 50px;
+  //  }
+  //
+  //  @media screen and (max-width: 655px) {
+  //    width: 185px;
+  //    height: 53px;
+  //  }
+  //}
 
   &_logotype-citycam {
     width: 210px;
