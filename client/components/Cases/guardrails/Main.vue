7b56c98a1eb6e8c778a1e3eab3b2987219836125
Antoneo-MD <anton.zaichenko@maddevs.io>
2022-01-24 19:56:08 +0600

    MAR-66. Added "The story behind the project" component

--- client/components/Cases/guardrails/Main.vue
+++ client/components/Cases/guardrails/Main.vue
@@ -1,16 +1,19 @@
 <template>
   <section>
     <About />
+    <StoryBehindProject />
   </section>
 </template>
 
 <script>
 import About from '@/components/Cases/guardrails/About'
+import StoryBehindProject from '@/components/Cases/guardrails/StoryBehindProject'
 
 export default {
   name: 'Main',
   components: {
     About,
+    StoryBehindProject,
   },
 }
 </script>
