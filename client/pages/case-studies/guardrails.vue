f8e7b7a5ffa6d40c981a75dc6cef1cb3341d0648
oh-cactus <roman.zh@maddevs.io>
2022-01-25 11:42:40 +0200

    Revert "MAR-66."
    This reverts commit b386f250452868bbcbbaa157fb7fb9b108425769.

--- client/pages/case-studies/guardrails.vue
+++ client/pages/case-studies/guardrails.vue
@@ -2,38 +2,27 @@
   <main class="case case--guardrails">
     <CaseHeader
       :logo="logo"
-      project="guardrails"
-      video-name="/videos/guardrails-banner.mp4"
+      project="clutch"
+      video-name="/videos/clutch-banner.mp4"
       video-fallback-path="/images/Cases/clutch/png/clutch-banner.png"
     >
-      <h1
-        slot="title"
-        class="case_header-title"
-      >
-        Enhancing the GuardRails solution
-      </h1>
-      <p
-        slot="description"
-        class="case_header-description"
-      >
-        Mad Devs assisted GuardRails an application security platform <br> to advance their product and increase its business value.
-      </p>
     </CaseHeader>
-    <!--    <Main />-->
     <Footer
-      link="/case-studies/clutch/"
-      class-name="clutch"
+      link="/case-studies/sir-john-monash-centre/"
+      class-name="sjmc"
     >
       <img
         slot="icon"
         v-lazy-load
-        :data-src="require(`@/assets/img/Studies/svg/clutch-footer.svg`)"
-        width="312"
-        height="138"
-        alt="Clutch"
-        class="case_logotype-clutch"
+        :data-src="require(`@/assets/img/Projects/svg/sjmc-black.svg`)"
+        width="195"
+        height="72"
+        alt="Better cities with CityCam"
+        class="case_logotype-citycam"
       >
-      The growth of Clutch.co
+      The maintenance of memory:
+      <br>
+      Sir John Monash Centre
     </Footer>
   </main>
 </template>
@@ -41,7 +30,6 @@
 <script>
 import CaseHeader from '@/components/Cases/shared/CaseHeader'
 import Footer from '@/components/Cases/shared/CaseFooter'
-// import Main from '@/components/Cases/clutch/Main'
 import { getMetadata, buildHead } from '@/data/seo'
 import animateOnScrollMixin from '@/mixins/animateOnScrollMixin'
 import headerMixin from '@/mixins/headerMixin'
@@ -50,8 +38,7 @@ export default {
   name: 'Clutch',
   components: {
     CaseHeader,
-    Footer, //    Main,
-
+    Footer,
   },
 
   nuxtI18n: false,
@@ -71,10 +58,10 @@ export default {
     return {
       logo: {
         width: 218,
-        height: 300,
-        folder: 'guardrails',
-        file: 'guardrails-logo',
-        alt: 'GUARDRAILS',
+        height: 63,
+        folder: 'clutch',
+        file: 'clutch-logo',
+        alt: 'A platform of client reviews and content',
       },
     }
   },
@@ -94,8 +81,8 @@ export default {
 @import '../../assets/styles/cases/media';
 
 .case {
-  &_header-guardrails {
-    background-color:  #323A45;
+  &_header-clutch {
+    background-color: rgba(8, 83, 126, 0.9);
   }
 
   &_header {
@@ -112,19 +99,19 @@ export default {
     }
   }
 
-  //&_guardrails-logo {
-  //  //width: 218px;
-  //  //height: 300px;
-  //
-  //  @media screen and (max-width: 1170px) {
-  //    margin-bottom: 50px;
-  //  }
-  //
-  //  @media screen and (max-width: 655px) {
-  //    width: 185px;
-  //    height: 53px;
-  //  }
-  //}
+  &_clutch-logo {
+    width: 218px;
+    height: 63px;
+
+    @media screen and (max-width: 1170px) {
+      margin-bottom: 50px;
+    }
+
+    @media screen and (max-width: 655px) {
+      width: 185px;
+      height: 53px;
+    }
+  }
 
   &_logotype-citycam {
     width: 210px;
