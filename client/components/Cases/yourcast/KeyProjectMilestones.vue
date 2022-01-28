8ec4eac325d9f66ab8ff0078af8486d7b2b88a59
vladDomino <vladislav.k@maddevs.io>
2022-01-28 20:40:26 +0600

    MAR-627. Remade the Key projects milestones block

--- client/components/Cases/yourcast/KeyProjectMilestones.vue
+++ client/components/Cases/yourcast/KeyProjectMilestones.vue
@@ -1,45 +1,128 @@
 <template>
-  <section class="container_regular">
-    <h3 class="case_title_h2 m-24_bottom media-m-12_bottom">
+  <section class="container_regular p-96_bottom p-96_top key-project-milestone">
+    <h3
+      v-prlx="animationSettingsTitle"
+      class="case_title_h2 m-48_bottom media-m-24_bottom"
+    >
       Key project milestones
     </h3>
-    <div class="m-96_bottom media-m-48_bottom case_key-project-milestones">
-      <CardProjectMilestone
-        v-for="projectMilestone in keyProjectMilestones"
-        :key="projectMilestone.title"
-        :title="projectMilestone.title"
-        :description="projectMilestone.description"
-        :background="projectMilestone.background"
-      />
+    <div
+      class="media-m-48_bottom case_key-project-milestones"
+    >
+      <div class="case_cards-container m-12_bottom">
+        <div
+          class="case_column"
+        >
+          <CardTechSolution
+            v-for="result in keyProjectMilestones.slice(0, 2)"
+            v-bind="result"
+            :key="result.icon"
+            folder="yourcast"
+            :icon="result.icon"
+          />
+        </div>
+        <div
+          v-prlx="animationSettingsCards"
+          class="case_column"
+        >
+          <div class="case_column-right">
+            <CardTechSolution
+              v-for="result in keyProjectMilestones.slice(2, 5)"
+              v-bind="result"
+              :key="result.icon"
+              folder="yourcast"
+              :icon="result.icon"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
-import CardProjectMilestone from '@/components/Cases/yourcast/CardProjectMilestone'
 import { keyProjectMilestones } from '@/data/caseYourcast'
+import CardTechSolution from '@/components/Cases/shared/CardTechSolution'
 
 export default {
   name: 'KeyProjectMilestones',
   components: {
-    CardProjectMilestone,
+    CardTechSolution,
   },
 
   data() {
     return {
       keyProjectMilestones,
+      animationSettingsTitle: {
+        direction: 'y',
+        speed: 0.3,
+        fromBottom: false,
+        reverse: true,
+        limit: {
+          min: -90,
+          max: 90,
+        },
+
+        mobileMaxWidth: 977,
+      },
+
+      animationSettingsCards: {
+        direction: 'y',
+        speed: 0.6,
+        fromBottom: false,
+        reverse: true,
+        limit: {
+          min: -240,
+          max: 240,
+        },
+
+        mobileMaxWidth: 977,
+      },
     }
   },
 }
 </script>
 
 <style lang="scss">
+.key-project-milestone {
+  overflow: hidden;
+}
 .case {
-  &_key-project-milestones {
-    @include grid(1fr, auto, 0, 20px);
+  &_cards-container {
+    @include grid(repeat(2, 1fr), auto, 24px, 0);
+    position: relative;
+  }
 
-    @media screen and (max-width: 768px) {
+  &_column {
+    @include grid(repeat(1, 1fr), repeat(2, minmax(min-content, max-content)), 0, 24px);
+    &-right {
+      @media screen and (min-width: 997px) {
+        position: absolute;
+        top: 0;
+        right: 0;
+      }
+      @include grid(repeat(1, 1fr), repeat(3, minmax(min-content, max-content)), 0, 24px);
+    }
+    ::v-deep{
+      .card-content{
+        align-self: start;
+      }
+    }
+  }
+
+  @media screen and (max-width: 976px) {
+    &_cards-container {
+      margin-bottom: 96px;
+      @include grid(repeat(1, 1fr), auto, 0, 10px);
+    }
+
+    &_column {
       grid-row-gap: 10px;
+    }
+  }
+  @media screen and (max-width: 768px) {
+    &_cards-container {
+      margin-bottom: 48px;
     }
   }
 }
--- client/data/caseYourcast.js
+++ client/data/caseYourcast.js
@@ -29,32 +29,31 @@ export const keyProjectMilestones = [
   {
     title: '2016',
     description:
-      'A database of movies with descriptions transformed into online cinema with a basic content management functionality.',
-
-    background: 'purple-light',
+      'As a pet project, Mad Devs developed a streaming service and open-sourced it, quickly finding a customer who wanted to expand the service for their business.',
+    icon: 'open-source',
   },
   {
     title: '2017',
     description: 'Live video streaming of popular TV channels became available.',
-    background: 'purple-primary',
+    icon: 'live-video',
   },
   {
     title: '2018',
     description: 'The project was redesigned to optimise resource consumption.',
-    background: 'purple-medium',
+    icon: 'optimization',
   },
   {
     lottieFileName: '',
     title: '2019',
     description: 'Added Video on Demand functionality.',
-    background: 'purple-dark',
+    icon: 'search',
   },
   {
     title: '2020',
     description:
       'User authorisation ensured a more personalised experience for each user. Custom filters, such as ‘favourites’, ‘already watched’ and ‘search’, made the service more viewer-friendly.',
 
-    background: 'black-primary-bg',
+    icon: 'user_circle_full_fill',
   },
 ]
 
