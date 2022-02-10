dc43634474f5b713b11b2e4fc46b5ed63898264d
vladDomino <vladislav.k@maddevs.io>
2022-02-10 19:53:17 +0600

    MAR-2869. Extended parallax functionality and rewrote it. Replaced the old parallax with my own in another type of cards.

--- client/plugins/mad-parallax.js
+++ client/plugins/mad-parallax.js
@@ -1,36 +1,96 @@
 import Vue from 'vue'
 
-Vue.directive('mad-parallax', {
-  bind(el, binding) {
-    const options = {
-      speed: binding.value?.speed || 0.1,
-    }
-    const elem = el
-    const mobileSize = 976
-    const parallaxScrollHander = () => {
-      const parentHeight = elem.parentElement.getBoundingClientRect().height
-      const pxToMove = parentHeight - elem.getBoundingClientRect().height
-      const elemInToView = window.innerHeight - (
-        elem.getBoundingClientRect().top + (elem.getBoundingClientRect().height / 2)
-      )
-      elem.style.transition = `transform ${options.speed}s ease-out`
+const parallaxScrollHandler = (elem, options) => {
+  if (elem.target !== document) {
+    const elemInToView = window.innerHeight - (
+      elem?.getBoundingClientRect().top + (elem?.getBoundingClientRect().height / options.startPoint)
+    )
+    const parentHeight = elem?.parentElement?.getBoundingClientRect().height
+    elem.style.transition = `transform ${options.speed}s ease-out`
+    if (!options.customMove) {
+      const pxToMove = parentHeight - elem?.getBoundingClientRect().height
       if (elemInToView < 0) {
-        elem.style.transform = 'translateY(0px)'
+        elem.style.transform = `translate${options.direction.toUpperCase()}(0px)`
       } else if (elemInToView + pxToMove + parentHeight < window.scrollY) {
-        elem.style.transform = `translateY(${pxToMove}px)`
+        elem.style.transform = `translate${options.direction.toUpperCase()}(${pxToMove}px)`
       }
       if (elemInToView >= 0 && elemInToView <= pxToMove) {
-        elem.style.transform = `translateY(${elemInToView}px)`
+        elem.style.transform = `translate${options.direction.toUpperCase()}(${elemInToView}px)`
+      }
+    } else {
+      if (elemInToView < 0) {
+        elem.style.transform = `translate${options.direction.toUpperCase()}(0px)`
+      } else if (elemInToView + options.maxMove + parentHeight < window.scrollY) {
+        elem.style.transform = `translate${options.direction.toUpperCase()}(${options.maxMove}px)`
+      }
+      if (elemInToView >= 0 && elemInToView <= Math.abs(options.maxMove)) {
+        if (options.reverse) {
+          elem.style.transform = `translate${options.direction.toUpperCase()}(-${elemInToView}px)`
+        }
       }
     }
-    window.addEventListener('scroll', parallaxScrollHander)
-    window.addEventListener('resize', () => {
-      if (window.innerWidth <= mobileSize) {
-        elem.style.transform = 'translateY(0px)'
-        window.removeEventListener('scroll', parallaxScrollHander)
-      } else {
-        window.addEventListener('scroll', parallaxScrollHander)
+  }
+}
+
+const checkIsMobileSize = (elem, options) => {
+  if (window.innerWidth <= options.mobileSize) {
+    elem.style.transform = `translate${options.direction.toUpperCase()}(0px)`
+    window.removeEventListener('scroll', parallaxScrollHandler)
+  } else {
+    window.addEventListener('scroll', parallaxScrollHandler)
+  }
+}
+
+let elems = []
+let options = {}
+
+const onScroll = () => {
+  for (const elem of elems) {
+    parallaxScrollHandler(elem.el, elem.options)
+  }
+}
+
+const onResize = () => {
+  for (const elem of elems) {
+    checkIsMobileSize(elem.el, elem.options)
+  }
+}
+
+Vue.directive('mad-parallax', {
+  bind(el, binding) {
+    options = {
+      speed: binding.value?.speed || 0.1,
+      direction: binding.value?.direction || 'y',
+      mobileSize: binding.value?.mobileSize || 976,
+      maxMove: binding.value?.maxMove || 0,
+      reverse: binding.value?.reverse || false,
+      customMove: binding.value?.customMove || false,
+      startPoint: binding.value?.startPoint || 1.5,
+      disabled: binding.value?.disabled || false,
+    }
+    if (!options.disabled) {
+      if (options.reverse) {
+        options.maxMove = -options.maxMove
       }
-    })
+      if (binding.value?.setIndentTop) {
+        el.style.position = 'relative'
+        el.style.top = `${Math.abs(options.maxMove)}px`
+      }
+      if (elems.length === 0) {
+        window.addEventListener('scroll', onScroll)
+        window.addEventListener('resize', onResize)
+      }
+      elems.push({
+        el,
+        options,
+      })
+    }
+  },
+  unbind(el) {
+    elems = elems.filter(element => element.el !== el)
+    if (elems.length === 0) {
+      window.removeEventListener('scroll', onScroll)
+      window.removeEventListener('resize', onResize)
+    }
   },
 })
