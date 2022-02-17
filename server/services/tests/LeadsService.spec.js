eb384e5fe9e75717f882b4b72eddcfdf3de8f6b2
Antoneo-MD <anton.zaichenko@maddevs.io>
2022-02-17 17:20:04 +0600

    MAR-2896. Updated tests

--- server/services/tests/LeadsService.spec.js
+++ server/services/tests/LeadsService.spec.js
@@ -93,7 +93,7 @@ describe('Leads service', () => {
         customfield_10063: 'company',
         customfield_10064: 'maddevs.io',
         customfield_10066: 'https://megadocnotprovided.com',
-        customfield_10166: { value: 'Partnership'},
+        customfield_10166: { value: 'Partnership' },
         issuetype: {
           id: '10082',
         },
