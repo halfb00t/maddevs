64cd4041bd2220230848cbc0f0fd2c6d5d831e23
Antoneo-MD <anton.zaichenko@maddevs.io>
2022-02-17 13:23:30 +0600

    MAR-2870-component-card. change cards to colored cards component on citycam page

--- client/components/Cases/citycam/WhyCityCam.vue
+++ client/components/Cases/citycam/WhyCityCam.vue
@@ -18,48 +18,13 @@
       around or an official in charge of traffic management. Consider the benefits of traffic cameras for three
       potential categories of users.
     </TextParagraph>
-    <div
-      class="case_cards m-24_bottom"
-    >
-      <div class="case_cards-group">
-        <Card
-          v-for="(card, i) in whyCityCamCards"
-          :id="`citycam-card-${i}`"
-          :key="card.title"
-          :class="`background-color-${card.color}`"
-        >
-          <div class="case_card-img">
-            <Picture
-              :width="396"
-              :height="200"
-              :file="card.img"
-              :alt="card.title || 'Image'"
-              folder="citycam"
-              extension="png"
-            />
-          </div>
-          <h3 class="case_title_h4 case_card-title m-24_top m-8_bottom">
-            {{ card.title }}
-          </h3>
-          <TextParagraph color="#ffffff">
-            {{ card.description }}
-          </TextParagraph>
-        </Card>
-      </div>
-      <Card
-        id="citycam-card-2"
-        class="background-color-blue-dark"
-      >
-        <h3 class="case_title_h4 case_card-title m-8_bottom">
-          City administrators
-        </h3>
-        <TextParagraph color="#ffffff">
-          They can identify weaknesses in the way traffic is currently organised by statistically analysing what
-          crossroads and road sections get clogged frequently. Adjustments, like making certain green lights last
-          longer, can solve congestion problems.
-        </TextParagraph>
-      </Card>
-    </div>
+
+    <ColoredCards
+      :cards-data="whyCityCamCards"
+      grid-type="two-one"
+      class="m-24_bottom"
+    />
+
     <TextParagraph class="m-96_bottom media-m-48_bottom">
       Providing these benefits takes cutting-edge technological solutions. The cameras need to require minimum energy
       supply and function continuously without frequent external inputs. It was a serious challenge to build them that
@@ -70,18 +35,16 @@
 
 <script>
 import TextParagraph from '@/components/Cases/shared/TextParagraph'
-import Picture from '@/components/Cases/shared/Picture'
-import Card from '@/components/Cases/shared/Card'
 import { whyCityCamCards, idCardList } from '@/data/caseCityCam'
 import IntersectionObserverMixin from '@/mixins/IntersectionObserverMixin'
 import setBackground from '@/helpers/setBackground'
+import ColoredCards from '@/components/Cases/shared/ColoredCards/ColoredCards'
 
 export default {
   name: 'WhyCityCam',
   components: {
     TextParagraph,
-    Card,
-    Picture,
+    ColoredCards,
   },
 
   mixins: [
