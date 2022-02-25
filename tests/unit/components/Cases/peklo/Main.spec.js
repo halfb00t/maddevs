e07c313ede9a2e780959675d94ff85fd6840fc1d
Antoneo-MD <anton.zaichenko@maddevs.io>
2022-02-25 15:00:16 +0600

    MAR-2922. Peklo: Added tests to dynamic imports

--- tests/unit/components/Cases/peklo/Main.spec.js
+++ tests/unit/components/Cases/peklo/Main.spec.js
@@ -2,6 +2,11 @@ import { render } from '@testing-library/vue'
 import { shallowMount } from '@vue/test-utils'
 import Main from '@/components/Cases/peklo/Main'
 
+const directives = {
+  'lazy-load': () => {},
+  'mad-parallax': () => {},
+}
+
 const stubs = ['About',
   'BenefitsFromPekloTool',
   'HowPekloToolWorks',
@@ -21,15 +26,31 @@ describe('Main component', () => {
 
     expect(container).toMatchSnapshot()
   })
-  describe('Dymanic imports', () => {
-    beforeEach(() => {
-      jest.resetModules()
-    })
-    it('should find text in dymanic imports', async () => {
-      const App = (await import('@/components/Cases/peklo/BenefitsFromPekloTool')).default
-      const wrapper = shallowMount(App)
+  describe('Dynamic imports Peklo component Main', () => {
+    it('should correctly import components', async () => {
+      const container = shallowMount(Main, {
+        mocks: {
+          $getMediaFromS3: () => 'img.jpg',
+        },
+        directives,
+      })
+      const About = await container.vm.$options.components.About.call()
+      const BenefitsFromPekloTool = await container.vm.$options.components.BenefitsFromPekloTool.call()
+      const HowPekloToolWorks = await container.vm.$options.components.HowPekloToolWorks.call()
+      const KeywordGrouping = await container.vm.$options.components.KeywordGrouping.call()
+      const ClusterisationAlgorithm = await container.vm.$options.components.ClusterisationAlgorithm.call()
+      const PekloToolUnderTheHood = await container.vm.$options.components.PekloToolUnderTheHood.call()
+      const Technologies = await container.vm.$options.components.Technologies.call()
+      const Team = await container.vm.$options.components.Team.call()
 
-      expect(wrapper.find('.case_title_h2').exists()).toBe(true)
+      expect(About.default.name).toBe('About')
+      expect(BenefitsFromPekloTool.default.name).toBe('BenefitsFromPekloTool')
+      expect(HowPekloToolWorks.default.name).toBe('HowPekloToolWorks')
+      expect(KeywordGrouping.default.name).toBe('KeywordGrouping')
+      expect(ClusterisationAlgorithm.default.name).toBe('ClusterisationAlgorithm')
+      expect(PekloToolUnderTheHood.default.name).toBe('PekloToolUnderTheHood')
+      expect(Technologies.default.name).toBe('Technologies')
+      expect(Team.default.name).toBe('MeetTheTeam')
     })
   })
 })
