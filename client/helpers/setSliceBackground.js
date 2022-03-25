99ee058201f18884189bfa04403caab9bacdb926
vladDomino <vladislav.k@maddevs.io>
2022-03-25 19:31:31 +0600

    MAR-3080. Add a list variation without a title. Fixed the problem of a cropped image when there is not enough text.

--- client/helpers/setSliceBackground.js
+++ client/helpers/setSliceBackground.js
@@ -7,10 +7,25 @@ export default function setSliceBackground(backgroundColor) {
       return '#f5f7f9'
     }
     case 'silver': {
-      return '#f5f7f9'
+      return '#f4f4f4'
     }
     case 'black': {
       return '#111213'
+    }
+    case 'red': {
+      return '#EC1C24'
+    }
+    case 'blue': {
+      return '#5159F6'
+    }
+    case 'yellow': {
+      return '#F7C744'
+    }
+    case 'purple': {
+      return '#9963F6'
+    }
+    case 'green': {
+      return '#00C05B'
     }
     default:
       return null
