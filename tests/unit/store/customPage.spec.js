e0a0787878ca74e6879ab239158ab7235e6e2eea
oh-cactus <roman.zh@maddevs.io>
2022-03-30 16:31:06 +0300

    MAR-3073. Getting og:image from prismic for customPages.

--- tests/unit/store/customPage.spec.js
+++ tests/unit/store/customPage.spec.js
@@ -86,6 +86,7 @@ describe('Custom page module mutations', () => {
       show_footer: false,
       meta_title: 'Meta title',
       meta_description: 'Meta description',
+      og_image: 'url',
       schema_org_snippets: [],
     },
   }
@@ -101,11 +102,11 @@ describe('Custom page module mutations', () => {
       showFooter: false,
       metaTitle: 'Meta title',
       metaDescription: 'Meta description',
+      ogImage: '',
       schemaOrgSnippet: schema,
     }
 
     mutations.SET_CUSTOM_PAGE(state, prismicResponse)
-
     expect(state).toEqual({
       ...defaultState(),
       customPage: expectedData,
