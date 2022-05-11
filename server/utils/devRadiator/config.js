321facda6e2b01ff20afad44e5442a21dd2d2108
Antoneo-MD <anton.zaichenko@maddevs.io>
2022-05-11 17:09:38 +0600

    MAR-3222. Added Dev Radiator

---
+++ server/utils/devRadiator/config.js
@@ -0,0 +1,54 @@
+const goals = {
+// types of goals:
+// "=" - good if metric is equal to the given value
+// "<" - good if metric less than the given value
+// ">" - good if metric larger than the given value
+
+  deploys: {
+    value: 2,
+    type: '>',
+  },
+  eslint: {
+    value: 0,
+    type: '=',
+  },
+  coverage: {
+    branches: {
+      value: 75,
+      type: '>',
+    },
+    functions: {
+      value: 78,
+      type: '>',
+    },
+    lines: {
+      value: 85,
+      type: '>',
+    },
+    statements: {
+      value: 85,
+      type: '>',
+    },
+    total: {
+      value: 90,
+      type: '>',
+    },
+  },
+}
+
+const messagesText = {
+  run: '\n'
+    + '____ _  _ _  _    ____ ____ ___  _ ____ ___ ____ ____ \n'
+    + '|__/ |  | |\\ |    |__/ |__| |  \\ | |__|  |  |  | |__/ \n'
+    + '|  \\ |__| | \\|    |  \\ |  | |__/ | |  |  |  |__| |  \\ \n'
+    + '                                                      \n',
+  successSend: '____ ____ ___  _ ____ ___ ____ ____    ____ ____ _  _ ___    ___ ____    ____ _    ____ ____ _  _ \n'
+    + '|__/ |__| |  \\ | |__|  |  |  | |__/    [__  |___ |\\ |  |      |  |  |    [__  |    |__| |    |_/  \n'
+    + '|  \\ |  | |__/ | |  |  |  |__| |  \\    ___] |___ | \\|  |      |  |__|    ___] |___ |  | |___ | \\_ \n'
+    + '                                                                                                  ',
+}
+
+module.exports = {
+  goals,
+  messagesText,
+}
---
+++ server/utils/devRadiator/utils/DeploysCount.js
@@ -0,0 +1,19 @@
+const axios = require('axios')
+const moment = require('moment')
+const { RADIATOR_GH_TOKEN } = require('../../../config')
+
+async function getDeploysCount() {
+  const startOfWeek = moment().startOf('week')
+  const endOfWeek = moment().endOf('week')
+
+  const ghPRsUrl = 'https://api.github.com/repos/maddevsio/maddevs/pulls?state=closed&base=master&head=staging&per_page=10'
+  const { data: pullRequests } = await axios.get(ghPRsUrl, { headers: { Authorization: `token ${RADIATOR_GH_TOKEN}` } })
+
+  const filtredPRs = pullRequests
+    .map(pr => ({ title: pr.title, merged_at: pr.merged_at }))
+    .filter(pr => moment(pr.merged_at).isBetween(startOfWeek, endOfWeek))
+
+  return filtredPRs.length
+}
+
+module.exports = { getDeploysCount }
