d46aae81dbcf6ac03fd19abd72ef8f503f40ac9b
Antoneo-MD <anton.zaichenko@maddevs.io>
2022-06-02 11:39:38 +0600

    MAR-3388. Added tests to FooterNavbar

---
+++ tests/unit/components/core/Footer/FooterNavbar.spec.js
@@ -0,0 +1,123 @@
+import { render } from '@testing-library/vue'
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import Vuex from 'vuex'
+import FooterNavbar from '@/components/core/Footer/FooterNavbar'
+import { footerNavigation } from './footerNavigationFixture'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+const recipeMock = jest.fn()
+recipeMock.mockReturnValue('footerMainNavigation')
+
+jest.mock('~/helpers/generatorUid')
+
+const mocks = {
+  $nuxt: {
+    $emit: jest.fn(),
+  },
+  $router: { push: jest.fn() },
+}
+
+const props = {
+  navigations: [
+    [
+      {
+        name: 'company',
+        label: 'Company',
+      },
+    ],
+    [
+      {
+        name: 'services',
+        label: 'Services',
+      },
+    ],
+    [
+      {
+        name: 'industries',
+        label: 'Industries',
+      },
+      {
+        name: 'clients',
+        label: 'Clients',
+      },
+      {
+        name: 'insights',
+        label: 'Insights',
+      },
+    ],
+  ],
+}
+const stubs = [
+  'NuxtLink',
+]
+
+const store = new Vuex.Store({
+  getters: {
+    footerMainNavigation: () => footerNavigation,
+  },
+})
+
+describe('FooterNavbar component', () => {
+  let wrapper
+  beforeEach(() => {
+    wrapper = shallowMount(FooterNavbar, {
+      propsData: props,
+      localVue,
+      store,
+      stubs,
+      mocks,
+    })
+  })
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should correct render footer navbar', () => {
+    const { container } = render(FooterNavbar, {
+      propsData: props,
+      localVue,
+      store,
+      stubs,
+    })
+    expect(container)
+      .toMatchSnapshot()
+  })
+  it('should correct render without navigation', () => {
+    expect(wrapper.vm.$options.props.navigations.default.call())
+      .toEqual([])
+  })
+
+  it('should correct emmit event changed-active-column from company', async () => {
+    await wrapper.find('.footer-nav-column--company')
+      .trigger('mouseenter')
+    expect(wrapper.emitted('changed-active-column'))
+      .toHaveLength(1)
+  })
+
+  it('should correct emmit event changed-active-column from services', async () => {
+    await wrapper.find('.footer-nav-column--services')
+      .trigger('mouseenter')
+    expect(wrapper.emitted('changed-active-column'))
+      .toHaveLength(1)
+  })
+
+  it('should correct emmit event changed-active-column from industries', async () => {
+    await wrapper.find('.footer-nav-column--industries')
+      .trigger('mouseenter')
+    expect(wrapper.emitted('changed-active-column'))
+      .toHaveLength(1)
+  })
+
+  it('should correct redirect to oth page', async () => {
+    wrapper.vm.goTo('services')
+    expect(mocks.$router.push)
+      .toHaveBeenCalledTimes(1)
+  })
+
+  it('should not redirect to oth page', async () => {
+    wrapper.vm.goTo('kek')
+    expect(mocks.$router.push)
+      .toHaveBeenCalledTimes(0)
+  })
+})
---
+++ tests/unit/components/core/Footer/footerNavigationFixture.js
@@ -0,0 +1,209 @@
+export const footerNavigation = {
+  company: {
+    name: 'company',
+    link: '/about/',
+    menus: [
+      {
+        name: 'Company',
+        routes: [
+          {
+            label: 'About Mad Devs',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.co/about/',
+            },
+          },
+          {
+            label: 'Team',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.io/team/',
+            },
+          },
+          {
+            label: 'Careers',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.io/careers/',
+            },
+          },
+          {
+            label: 'Contacts',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.io/contact-us/',
+            },
+          },
+          {
+            label: 'FAQ',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.io/faq/',
+            },
+          },
+        ],
+      },
+      {
+        name: 'Approach',
+        routes: [
+          {
+            label: 'Development process',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.io/development-process/',
+            },
+          },
+          {
+            label: 'Delivery models',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.io/delivery-models/',
+            },
+          },
+          {
+            label: 'Private Policy',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.io/privacy/',
+            },
+          },
+          {
+            label: 'Sustainability Policy',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.io/sustainability-policy/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  services: {
+    name: 'services',
+    link: '/services/',
+    menus: [
+      {
+        name: 'Software development',
+        routes: [
+          {
+            label: 'Frontend development',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.io/frontend-development/',
+            },
+          },
+          {
+            label: 'Backend development',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.io/backend-development/',
+            },
+          },
+          {
+            label: 'Quality assurance',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.io/software-testing/',
+            },
+          },
+          {
+            label: 'DevOps',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.io/devops/',
+            },
+          },
+          {
+            label: 'Mobile development',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.io/mobile-development/',
+            },
+          },
+          {
+            label: 'Design',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.io/design/',
+            },
+          },
+          {
+            label: 'Project management',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.io/project-management/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  clients: {
+    name: 'clients',
+    link: '',
+    menus: [
+      {
+        name: null,
+        routes: [
+          {
+            label: 'Case Studies',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.io/case-studies/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  insights: {
+    name: 'insights',
+    link: '',
+    menus: [
+      {
+        name: null,
+        routes: [
+          {
+            label: 'IT Blog',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.io/blog/',
+            },
+          },
+          {
+            label: 'Open Source',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.io/open-source/',
+            },
+          },
+          {
+            label: 'Ebooks',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.io/ebooks/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  industries: {
+    name: 'industries',
+    link: '',
+    menus: [
+      {
+        name: null,
+        routes: [
+          {
+            label: 'Transportation',
+            link: {
+              link_type: 'Web',
+              url: 'https://maddevs.io/transportation/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+}
