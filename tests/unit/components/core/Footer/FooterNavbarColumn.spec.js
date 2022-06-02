2e2446a024f6c0a77033dd5cd313811bfbfe1e8c
Antoneo-MD <anton.zaichenko@maddevs.io>
2022-06-02 12:56:24 +0600

    MAR-3388. Added tests to FooterNavbarColumn

---
+++ tests/unit/components/core/Footer/FooterNavbarColumn.spec.js
@@ -0,0 +1,32 @@
+import { render } from '@testing-library/vue'
+import { shallowMount } from '@vue/test-utils'
+
+import FooterNavbarColumn from '@/components/core/Footer/FooterNavbarColumn'
+import { footerNavigation } from './footerNavigationFixture'
+
+jest.mock('~/helpers/generatorUid')
+
+const stubs = ['FooterNavbarMenu']
+
+const props = {
+  menus: footerNavigation.company.menus,
+  name: footerNavigation.company.name,
+}
+describe('FooterNavbarColumn', () => {
+  it('should correct render footer navbar', () => {
+    const { container } = render(FooterNavbarColumn, {
+      stubs,
+      props,
+    })
+    expect(container)
+      .toMatchSnapshot()
+  })
+  it('should correct render without data', () => {
+    const wrapper = shallowMount(FooterNavbarColumn, {
+      stubs,
+      propsData: props,
+    })
+    expect(wrapper.vm.$options.props.menus.default.call())
+      .toEqual([])
+  })
+})
