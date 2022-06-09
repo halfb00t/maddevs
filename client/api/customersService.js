c1d7bef87c81f8e401c9a1a53589d1c11af97fcf
Rail Batyrshin <rail.batyrshin.js@gmail.com>
2022-06-09 18:28:32 +0600

    MAR-3331. Used array destructurisation

--- client/api/customersService.js
+++ client/api/customersService.js
@@ -1,7 +1,8 @@
 export const getAllCustomers = async prismic => {
   try {
     const { data: { body } } = await prismic.api.getByUID('customerstype', 'list-of-customer-logos')
-    return body[0].items
+    const [customers] = body
+    return customers.items
   } catch (error) {
     return error
   }
