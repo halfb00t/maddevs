9b3b99407b1c67161307f0ca3d7b7cf5a8681242
Antoneo-MD <anton.zaichenko@maddevs.io>
2022-06-17 04:44:58 +0600

    MAR-3398. Added new variation for WeCreateProjectsFroWithMap slice. Added screenshots to slicemachine. Fixed tests for Default variation. Updated snapshots

--- client/data/projectsFor.js
+++ client/data/projectsFor.js
@@ -44,4 +44,63 @@ export const industriesEntries = industries.reduce((acc, industry) => {
   if (last.length === 1) return [...acc.slice(0, acc.length - 1), [...last, industry]]
   if (last.length === 2) return [...acc, [industry]]
   return acc
-}, [])
+}, [])// todo remove this component when the new slice variation is finished
+
+export const industriesForMapVariation = [
+  {
+    iconID: 'blockchain-and-crypto',
+    text: 'Blockchain and Crypto',
+  },
+  {
+    iconID: 'ecommerce',
+    text: 'Ecommerce',
+  },
+  {
+    iconID: 'serverless',
+    text: 'Serverless',
+  },
+  {
+    iconID: 'transportation',
+    text: 'Transportation',
+  },
+  {
+    iconID: 'digital-transformation',
+    text: 'Digital transformation',
+  },
+  {
+    iconID: 'cloud-tech',
+    text: 'CloudTech',
+  },
+  {
+    iconID: 'ad-tech',
+    text: 'AdTech',
+  },
+  {
+    iconID: 'e-learning',
+    text: 'E-learning',
+  },
+  {
+    iconID: 'content-streaming',
+    text: 'Content Streaming',
+  },
+  {
+    iconID: 'health-tech',
+    text: 'HealthTech',
+  },
+  {
+    iconID: 'ed-tech',
+    text: 'EdTech',
+  },
+  {
+    iconID: 'iots',
+    text: 'IoTs',
+  },
+  {
+    iconID: 'fintech',
+    text: 'Fintech',
+  },
+  {
+    iconID: 'byod',
+    text: 'BYOD',
+  },
+]
