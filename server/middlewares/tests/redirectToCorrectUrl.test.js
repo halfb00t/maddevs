47906e3f6acbe2e972a2c559ae834e22d5cc9d2a
vladDomino <vladislav.k@maddevs.io>
2022-06-22 16:05:17 +0600

    MAR-3580. Fixed unit test.

--- server/middlewares/tests/redirectToCorrectUrl.test.js
+++ server/middlewares/tests/redirectToCorrectUrl.test.js
@@ -32,4 +32,19 @@ describe('redirectToCorrectUrl', () => {
     redirectToCorrectUrl(req, res, next)
     expect(res.redirect).toHaveBeenCalledWith(301, 'https://maddevs.io/test-redirects/success')
   })
+
+  it('if hostname blog.maddevs.io will be redirect to https://maddevs.io/blog/', () => {
+    req.headers.host = 'blog.maddevs.io'
+    req.hostname = 'blog.maddevs.io'
+    redirectToCorrectUrl(req, res, next)
+    expect(res.redirect).toHaveBeenCalledWith(301, 'https://maddevs.io/blog/')
+  })
+
+  it('if no hostname blog.maddevs.io and no url will be call next', () => {
+    req.headers.host = 'maddevs.io'
+    req.hostname = ''
+    req.url = ''
+    redirectToCorrectUrl(req, res, next)
+    expect(next).toHaveBeenCalledTimes(1)
+  })
 })
