f0c5a3f03dd4990d2f997f9070083e342ae63098
Rail Batyrshin <rail.batyrshin.js@gmail.com>
2022-07-15 10:50:15 +0600

    MAR-3702. Deleted writeFile unnecessaty function

--- utils/getRoutes.js
+++ utils/getRoutes.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import { writeFile } from 'fs'
 import { getNotAllowedRoutes } from './getNotAllowedRoutes'
 
 export const CUSTOM_PAGE_ROUTES = [
@@ -62,10 +61,6 @@ const getDynamicRoutes = async () => {
   // Getting posts of all types from prismic
   const { ref } = prismicData.data.refs[0]
   const prismicPosts = await getPosts(`${process.env.NODE_PRISMIC_API}/documents/search?ref=${ref}#format=json`)
-
-  writeFile('prismic.json', JSON.stringify(prismicPosts, null, 2), () => {
-    console.log('Prismic posts saved')
-  })
 
   // Creating a list of routes
   const blogPageRoutes = prismicPosts
