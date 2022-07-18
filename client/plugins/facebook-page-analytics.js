a50e426e681ab6a1a352796f94a88dc85f2306de
vladDomino <vladislav.k@maddevs.io>
2022-07-18 17:29:49 +0600

    MAR-3176. Refactor facebook-page-analytics plugin after review.

--- client/plugins/facebook-page-analytics.js
+++ client/plugins/facebook-page-analytics.js
@@ -1,15 +1,8 @@
-export default ctx => {
-  const hostNameProd = 'maddevs.io'
-  const hostNameStaging = 'maddevs.co'
-  const originProd = 'https://maddevs.io'
-  const originStaging = 'https://maddevs.co'
+import { isMatchMainDomain } from '@/helpers/isMatchMainDomain'
 
+export default ctx => {
   if (!process?.client) return
-  if (window.location.hostname === hostNameProd
-    || window.location.hostname === hostNameStaging
-    || window.location.origin === originProd
-    || window.location.origin === originStaging
-  ) {
+  if (isMatchMainDomain(window.location.origin)) {
     const { router, $fb } = ctx.app
 
     router.afterEach(() => {
