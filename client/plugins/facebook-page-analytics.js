91a8b95a2a8c250b2c31443aa66a76436b737a6c
vladDomino <vladislav.k@maddevs.io>
2022-07-18 17:23:20 +0600

    MAR-3176. Refactor facebook-page-analytics plugin after review.

--- client/plugins/facebook-page-analytics.js
+++ client/plugins/facebook-page-analytics.js
@@ -1,21 +1,21 @@
-import Vue from 'vue'
-
 export default ctx => {
-  if (process.client) {
-    if (window.location.hostname === 'maddevs.io'
-      || window.location.hostname === 'maddevs.co'
-      || window.location.origin === 'https://maddevs.co'
-      || window.location.origin === 'https://maddevs.io'
-    ) {
-      const { router } = ctx.app
-      const { $fb } = ctx.app
-      router.afterEach(() => {
-        Vue.nextTick(() => {
-          setTimeout(() => {
-            $fb.fbq('trackCustom', 'PageView 15sec')
-          }, 15 * 1000)
-        })
-      })
-    }
+  const hostNameProd = 'maddevs.io'
+  const hostNameStaging = 'maddevs.co'
+  const originProd = 'https://maddevs.io'
+  const originStaging = 'https://maddevs.co'
+
+  if (!process?.client) return
+  if (window.location.hostname === hostNameProd
+    || window.location.hostname === hostNameStaging
+    || window.location.origin === originProd
+    || window.location.origin === originStaging
+  ) {
+    const { router, $fb } = ctx.app
+
+    router.afterEach(() => {
+      setTimeout(() => {
+        $fb.fbq('trackCustom', 'PageView 15sec')
+      }, 15 * 1000)
+    })
   }
 }
